1b07056803e9371ff3fd4a986583aebaca317e91
riddhiparmar98 <riddhipr@itpathsolutions.co.in>
2022-07-29 12:14:18 +0530

    updated

--- src/Component/Input.js
+++ src/Component/Input.js
@@ -4,7 +4,7 @@ import TextError from "./TextError";
 
 function input({ label, name, ...rest }) {
   return (
-    <div className="mb-3">
+    <div className="mb-5">
       <Field {...{ name, id: name }} {...rest} />
       <ErrorMessage {...{ name }} component={TextError} />
     </div>
