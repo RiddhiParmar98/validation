494170cf34c2ef75f173b7c02ec6e754295a6a2c
riddhiparmar98 <riddhipr@itpathsolutions.co.in>
2022-08-29 18:58:57 +0530

    File Error Msg

--- src/Component/File.js
+++ src/Component/File.js
@@ -1,15 +1,17 @@
+import { ErrorMessage } from "formik";
+import TextError from "./TextError";
 import React from "react";
 
-
 function File(props, ref) {
-  const { label, name, value, handleClick, ...rest } = props
+  const { label, name, value, handleClick, ...rest } = props;
   return (
-    <div className="flex">
-      <input {...{ name, id: name }} ref={ref} {...rest} />
-     
-    </div>
+    <>
+      <div className="flex">
+        <input {...{ name, id: name }} ref={ref} {...rest} />
+      </div>
+      <ErrorMessage {...{ name }} component={TextError} />
+    </>
   );
-  
 }
 
 export default React.forwardRef(File);
